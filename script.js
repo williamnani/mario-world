29e1e1747997ceb71f56dfc176e057eade441364
williamnani <williamnani22@gmail.com>
2024-01-20 13:58:55 -0300

    form

--- script.js
+++ script.js
@@ -2,12 +2,18 @@
  let formulario = document.querySelector(".fale-conosco")
  let mascara = document.querySelector(".mascara-form")
 
- function cliqueiNoBotao() {
-   formulario.style.left = "700px"
-   mascara.style.visibility = "visible"
- }
+function cliqueiNoBotao() {
+  // Verifica se a largura da janela é menor que 600 pixels
+  if (window.innerWidth < 600) {
+    formulario.style.left = "50%" // Ajusta para 5vw em modo responsivo
+    formulario.style.transform = "translateX(-50%)"
+  } else {
+    formulario.style.left = "37%" // Mantém como 9vw em outros casos
+  }
+  mascara.style.visibility = "visible"
+}
 
  function sumirFormulario() {
-   formulario.style.left = "-400px"
+   formulario.style.left = "-330px"
    mascara.style.visibility = "hidden"
  }
